18f49d23d4dfa8c67185943c4da52acd9f1ada5d
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 18:30:18 +0300

    Update Main.java

--- src/main/java/ru/mirea/dolgov/RickandMorty/Main.java
+++ src/main/java/ru/mirea/dolgov/RickandMorty/Main.java
@@ -1,4 +1,14 @@
 package ru.mirea.dolgov.RickandMorty;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            RickAndMortyClient client = new RickAndMortyClient();
+            client.findEpisodeWithMaxCharacters();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
