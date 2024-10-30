79dfb700a4cb8ec3f594e04e0d84559b3c67d183
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 18:31:19 +0300

    Update RickAndMortyService.java

--- src/main/java/ru/mirea/dolgov/RickandMorty/RickAndMortyService.java
+++ src/main/java/ru/mirea/dolgov/RickandMorty/RickAndMortyService.java
@@ -1,4 +1,10 @@
 package ru.mirea.dolgov.RickandMorty;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RickAndMortyService {
+    @GET("episode")
+    Call<Result> getEpisodes();
 }
+
