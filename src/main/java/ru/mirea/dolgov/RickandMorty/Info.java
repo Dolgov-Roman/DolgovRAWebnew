72500877227fd1b0a55c780a63f779c15cc3dc84
Долгов Роман Андреевич <rad2312@mail.ru>
2024-10-30 18:30:00 +0300

    Update Info.java

--- src/main/java/ru/mirea/dolgov/RickandMorty/Info.java
+++ src/main/java/ru/mirea/dolgov/RickandMorty/Info.java
@@ -1,4 +1,36 @@
 package ru.mirea.dolgov.RickandMorty;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Info {
+    @JsonProperty("count")
+    private int count;
+
+    @JsonProperty("pages")
+    private int pages;
+
+    @JsonProperty("next")
+    private String next;
+
+    @JsonProperty("prev")
+    private String prev;
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public String getPrev() {
+        return prev;
+    }
 }
+
